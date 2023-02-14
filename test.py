62135fbb8c081a9f95e7d31924810c51c2d62193
tpdud3406 <60971971+tpdud3406@users.noreply.github.com>
2023-02-14 11:37:25 +0900

    pytorch tensors

--- test.py
+++ test.py
@@ -40,4 +40,27 @@ r = (torch.rand(2,2) - 0.5) * 2 # -1과 1사이의 값
 print("랜덤 행렬값, r:")
 print(r)
 
+# 절댓값
+print('\nr의 절댓값:')
+print(torch.abs(r))
+
+# 삼각함수
+print('\nr의 사인 함수:') # sin x
+print(torch.sin(r))
+print('\nr의 역 사인 함수:') # arcsin x
+print(torch.asin(r))
+
+# 선형대수 연산
+# 행렬식 (determinant): 행렬을 대표하는 값. 2x2일때 행렬의 요소 값이 a,b,c,d 일 때 행렬 식은 ad - bc 이다.
+print('\nr의 행렬식:')
+print(torch.det(r))
+# 특이값 분해
+print('\nr의 특이값 분해:')
+print(torch.svd(r))
+
+# 통계 및 집합 연산 등
+print('\nr의 평균 및 표준편차:')
+print(torch.std_mean(r))
+print('\nr의 최대값:')
+print(torch.max(r))
 
